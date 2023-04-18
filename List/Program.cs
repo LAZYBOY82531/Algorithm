033dbc48be9b0ef12c2e02674eea6b489f2a4264
jim raynor <fallere82531@gmail.com>
2023-04-18 17:40:07 +0900

    feat: Add description

--- List/Program.cs
+++ List/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace List
+﻿using Apache.NMS.ActiveMQ.Commands;
+
+namespace List
 {
     internal class Program
 	{
@@ -34,7 +36,7 @@
 		 ******************************************************/
 
 		// <List의 사용>
-		void List()
+		void Listt()
 		{
 			List<string> list = new List<string>();
 
@@ -55,12 +57,40 @@
 			int findIndex = list.FindIndex(x => x.Contains('0'));
 		}
 
+		// <List의 시간복잡도>
+		// 접근		탐색		삽입		삭제
+		// O(1)		O(n)	O(n)	O(n)
+
+
 
 		static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-			int[] a = new int[10];
-			Array.Resize(ref a, 100);
-        }
+			List<string> list = new List<string>();
+
+			list.Add("1번 데이터");
+			list.Add("2번 데이터");
+			list.Add("3번 데이터");
+			list.Add("4번 데이터");
+			list.Add("5번 데이터");
+
+			string value;
+			value = list[0];
+			value = list[1];
+			value = list[2];
+			value = list[3];
+			value = list[4];
+
+			list[0] = "5번 데이터";
+			list[1] = "4번 데이터";
+			list[2] = "3번 데이터";
+			list[3] = "2번 데이터";
+			list[4] = "1번 데이터";
+
+			list.Remove("3번 데이터");
+			list.Remove("2번 데이터");
+
+			string? findValue = list.Find(x => x.Contains('4'));
+			int findIndex = list.FindIndex(x => x.Contains('1'));
+		}
     }
 }
